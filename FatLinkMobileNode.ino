6718a35ef2ca39b0c84b3d1cd2b841883ff50c0f
Mikael Falkvidd git@mjo.se <git@mjo.se>
2017-03-17 19:54:49 +0100

    Initial commit

---
+++ FatLinkMobileNode.ino
@@ -0,0 +1,20 @@
+/*
+   Sends GPS position + time + configurable message
+   Log sent and received messages with timestamp, snr and rssi
+   Web GUI
+     Set send interval
+     Set message text
+     Show current position + time
+     Show live lov (sebsocket?) - buffer last x messages
+     Fetch entire log file
+     Clear/rotate log file
+     Set spreading factor
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
